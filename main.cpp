c3bb3de5e6f15a0dff1b1e49cef7223e82a5ae62
siffkroete <iwanp@gmx.ch>
2023-02-22 19:16:29 +0100

    boost hinzugefügt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include "my_lib/foo.h"
 
+#include <boost/array.hpp>
+
 using namespace std;
 
 int main(int argc, char** argv){
     foo();
-    
+     boost::array<int, 10> arr
+        = { { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } };
+    for (int i = 0; i < 10; i++) {
+        cout << "Geek Rank is :" << arr[i] << "*"
+             << "\n";
+    }
     return 0;
 }
